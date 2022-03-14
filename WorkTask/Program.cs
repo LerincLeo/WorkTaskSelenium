2e702a4a55b407557195e83adc01d003d833908b
LerincLeo <jazzo25512@gmail.com>
2022-03-14 01:52:46 +0100

    3rd
    Izrada zadatka 3/4

--- WorkTask/Program.cs
+++ WorkTask/Program.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
 
 namespace WorkTask
 {
     class Program
     {
-        static void Main(string[] args)
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void BrowserCheckup()
         {
+            //Open the Chrome browser and go to selected site...
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }        
+        [Test,Category("Clear")]
+        public void ClearLogIn()
+        {
+            //Clicking the login button.
+            SeleniumSetClass.ElementClick(driver, "//*[@id='login-button']", "XPath");
+        }
+        [Test, Category("Wrong")]
+        public void WrongUser()
+        {   //Entering a wrong username.
+            SeleniumSetClass.ElementText(driver, "//*[@id='user-name']", "wrong_user", "XPath");
+            //Entering a wrong password.
+            SeleniumSetClass.ElementText(driver, "//*[@id='password']", "bad_sauce", "XPath");
+            //Clicking the login button.
+            SeleniumSetClass.ElementClick(driver, "//*[@id='login-button']", "XPath");
+        }
+        [Test, Category("Correct")]
+        public void LogInWithUser()
+        {   //Entering a correct username.
+            SeleniumSetClass.ElementText(driver, "//*[@id='user-name']", "standard_user", "XPath");
+            //Entering a correct password.
+            SeleniumSetClass.ElementText(driver, "//*[@id='password']", "secret_sauce", "XPath");
+            //Clicking the login button.
+            SeleniumSetClass.ElementClick(driver, "//*[@id='login-button']", "XPath");
+        }
+        [TearDown]
+        public void BrowserCleanup()
+        {   
+            //Closing the program...
+            Thread.Sleep(5000);
+            driver.Close();
         }
     }
 }
---
+++ WorkTask/SeleniumSetClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WorkTask
+{
+    class SeleniumSetClass
+    {
+        //Quick use of methods (Driver, name of the element(Id,Name), text we want to put in the textbox, type of the element)
+        public static void ElementText(IWebDriver driver, string element, string value, string elementType)
+        {
+            //Searching the element by (Id, Name or Xpath) and inputing the value...
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            if (elementType == "XPath")
+                driver.FindElement(By.XPath(element)).SendKeys(value);
+        }
+        //Quick use of methods (Driver, name of the element(Id,Name), type of the element)
+        public static void ElementClick(IWebDriver driver, string element, string elementType)
+        {
+            //Searching the element by (Id, Name or Xpath) and clicking it...
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).Click();
+            if (elementType == "XPath")
+                driver.FindElement(By.XPath(element)).Click();
+        }
+
+    }
+}
